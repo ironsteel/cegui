1bc471b13bd246a3ee0386b0e75fa506b25dafb1
Rangel Ivanov <rangelivanov88@gmail.com>
2012-09-13 21:31:50 +0300

    Added cmake script for finding egl
    Link OpenGLES Renderer Module with EGL

---
+++ cmake/FindEGL.cmake
@@ -0,0 +1,23 @@
+################################################################################
+# Custom cmake module for CEGUI to find OpenGL ES (1.1) 
+#
+# Placeholder module to create some vars we can manually set.
+# Later will create a proper tests (or steal them, anyway)
+################################################################################
+include(FindPackageHandleStandardArgs)
+
+find_path(EGL_H_PATH NAMES EGL/egl.h)
+find_library(EGL_LIB NAMES EGL)
+mark_as_advanced(EGL_H_PATH EGL_LIB)
+
+find_package_handle_standard_args(EGL DEFAULT_MSG EGL_LIB EGL_H_PATH)
+
+# set up output vars
+if (EGL_FOUND)
+    set (EGL_INCLUDE_DIR ${OPENGLES_H_PATH})
+    set (EGL_LIBRARIES ${EGL_LIB})
+else()
+    set (OPENGLES_INCLUDE_DIR)
+    set (OPENGLES_LIBRARIES)
+endif()
+
